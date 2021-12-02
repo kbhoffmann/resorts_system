2a5237ee9b6cc19bba7cabc50a06761da51472af
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-02 15:41:33 -0700

    add instructor parent index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
   get '/resorts', to: 'resorts#index'
   get '/resorts/:id', to: 'resorts#show'
   get '/resorts/:resort_id/pass_holders', to: 'resort_pass_holders#index'
 
+  get '/instructors', to: 'instructors#index'
+
   get '/pass_holders', to: 'pass_holders#index'
   get '/pass_holders/:id', to: 'pass_holders#show'
 
+
   get '/resorts/new', to: 'resorts#new'
   post '/resorts', to: 'resorts#create'
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_223529) do
+ActiveRecord::Schema.define(version: 2021_12_02_223031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.string "subject"
+    t.boolean "teaches_children"
+    t.integer "years_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pass_holders", force: :cascade do |t|
     t.string "name"
