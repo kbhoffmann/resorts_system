8110322d28f722612c57e6d341efa368889c5513
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-02 15:06:15 -0700

    add created_at to resorts index view and feature test

--- spec/models/resort_spec.rb
+++ spec/models/resort_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Resort do
   it {should have_many :pass_holders}
-  
+
   describe 'Resort index' do
-    # As a visitor
-    # When I visit the parent index,
-    # I see that records are ordered by most recently created first
-    # And next to each of the records I see when it was created
+
     it 'shows resorts ordered by most recently created' do
       breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false )
       copper = Resort.create!(name: 'Copper', city: 'Frisco', runs: 15, ski_only: true)
@@ -16,7 +13,6 @@ RSpec.describe Resort do
       expect(Resort.ordered_by_created_at).to eq([breck, copper, seven_springs])
     end
 
-    it 'shows when resort was created' do
   end
 
 
