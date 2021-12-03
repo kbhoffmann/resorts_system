d57dff5ca8095a58592514ed0f1d37aa58cb9922
Hannah Warren <hannahkwarren@gmail.com>
2021-12-02 21:23:23 -0500

    Add students show feature test

---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'the student show page' do
+
+  it 'displays the student attributes' do
+    hans = Instructor.create!(name: "Hans", subject: "skiing", teaches_children: true, years_experience: 30)
+    gretchen = hans.students.create!(name: 'Gretchen', age: 20, subject: "cross-country skiing", returning_student: true, level:"advanced")
+    tristan = hans.students.create!(name: 'Tristan', age: 14, subject: "snowboarding", returning_student: false, level:"advanced")
+
+    visit "/students/#{gretchen.id}"
+
+    expect(page).to have_content(gretchen.id)
+    expect(page).to have_content(gretchen.name)
+    expect(page).to have_content(gretchen.subject)
+    expect(page).to have_content(gretchen.age)
+    expect(page).to have_content(gretchen.level)
+    expect(page).to have_content(gretchen.returning_student)
+    expect(page).to have_content(gretchen.created_at)
+    expect(page).to have_content(gretchen.updated_at)
+
+    expect(page).to_not have_content(tristan.name)
+  end
+end
