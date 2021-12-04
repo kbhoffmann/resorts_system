f921a3944b85a335d3e0b3601cfe704686d04d52
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-03 20:12:08 -0700

    Test: add tests to show link to students for instructor id and test for showing number of students for a particular instructor

--- spec/features/instructors/show_spec.rb
+++ spec/features/instructors/show_spec.rb
@@ -17,4 +17,29 @@ RSpec.describe 'the instructors show page' do
     expect(page).to have_content(instructor.updated_at)
   end
 
+  it 'shows the count of number of students for the instructor' do
+    hans = Instructor.create!(name: "Hans", subject: "skiing", teaches_children: true, years_experience: 30)
+    rob = Instructor.create!(name: "Hans", subject: "snowboarding", teaches_children: false, years_experience: 25)
+    gretchen = hans.students.create!(name: 'Gretchen', age: 20, subject: "skiing", returning_student: true, level:"advanced")
+    tristan = hans.students.create!(name: 'Tristan', age: 34, subject: "skiing", returning_student: false, level:"advanced")
+
+    visit "/instructors/#{hans.id}"
+
+    expect(page).to have_content("Number of Students: #{hans.students.count}")
+    expect(page).to_not have_content("Number of Students: #{rob.students.count}")
+  end
+
+  it 'has a link to students for the instructor' do
+    hans = Instructor.create!(name: "Hans", subject: "skiing", teaches_children: true, years_experience: 30)
+    rob = Instructor.create!(name: "Hans", subject: "snowboarding", teaches_children: false, years_experience: 25)
+
+    visit "/instructors/#{hans.id}"
+
+    expect(page).to have_link("Students for this Instructor")
+
+    click_on "Students for this Instructor"
+
+    expect(current_path).to eq("/instructors/#{hans.id}/students")
+    expect(current_path).to_not eq("/instructors/#{rob.id}/students")
+  end
 end
