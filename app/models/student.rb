9ab46e798efda553997accaea4d534a9ba47ca8f
Hannah Warren <hannahkwarren@gmail.com>
2021-12-05 10:27:01 -0500

    Refactor Child Index to show only true records

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :instructor
+
+  def self.show_only_bool_true
+    where(returning_student: true)
+  end
 end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Student model" do
+
+
+  it 'returns only students with boolean returning_student value true' do
+    gretchen = Instructor.create!(name: 'Gretchen', subject: "cross-country skiing", teaches_children: true, years_experience:10)
+    tommy = gretchen.students.create!(name: 'Tommy', subject: "cross-country skiing", returning_student: true, level: "intermediate")
+    dana = gretchen.students.create!(name: 'Dana', subject: "cross-country skiing", returning_student: true, level: "intermediate")
+    jim = gretchen.students.create!(name: 'Jim', subject: "cross-country skiing", returning_student: false, level: "intermediate")
+
+    expect(Student.show_only_bool_true).to eq( [tommy, dana])
+
+  end
+
+
+end
