ab8b3cef98f8b5ccae959dd05c5663b5da63e7e2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-04 18:09:54 -0700

    remove comments

--- spec/features/resorts/pass_holders/new_spec.rb
+++ spec/features/resorts/pass_holders/new_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'the Pass Holder Creation' do
     visit "/resorts/#{resort.id}/resort_pass_holders"
 
     click_link('New Passholder')
-    # new_resort_id = Resort.last.id
+  
     expect(current_path).to eq("/resorts/#{resort.id}/resort_pass_holders/new")
   end
 
@@ -28,26 +28,3 @@ RSpec.describe 'the Pass Holder Creation' do
     expect(page).to have_content(true)
   end
 end
-
-# User Story 13, REsort Passholder Creation (x2)
-# ---------------------------------------
-# As a visitor
-# When I visit a REsort Passholders Index page*
-# Then I see a link to add a new passholder for that resort "Create Passholder"*
-# When I click the link*
-# I am taken to '/resorts/:resort_id/passholder_table_name/new' where I see a form to add a new adoptable passholder*
-# When I fill in the form with the passholder's attributes: *
-# And I click the button "Create Passholder" *
-# Then a `POST` request is sent to '/resorts/:resort_id/passholder_table_name',
-# a new passholder object/row is created for that resort,
-# and I am redirected to the REsort Passholders Index page where I can see the new passholder listed
-
-
-# t.string "name"
-# t.integer "age"
-# t.string "level"
-# t.boolean "season_pass"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# t.bigint "resort_id"
-# t.index ["resort_id"], name: "index_pass_holders_on_resort_id"
