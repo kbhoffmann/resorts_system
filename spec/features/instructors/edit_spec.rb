b17a1c9529b954923a06954871c7455b812d6d05
Hannah Warren <hannahkwarren@gmail.com>
2021-12-05 10:04:36 -0500

    Add parent update from show page - previously only on the index page

--- spec/features/instructors/edit_spec.rb
+++ spec/features/instructors/edit_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Instructor edit' do
 
-  it 'links to the edit page' do
+  it 'links to the edit page from parent show page' do
+    instructor = Instructor.create!(name: "Olga", subject: "skiing", teaches_children: false, years_experience: 5)
+    visit "/instructors/#{instructor.id}"
+
+    click_link("Update #{instructor.name}")
+    expect(current_path).to eq("/instructors/#{instructor.id}/edit")
+  end
+
+  it 'links to the edit page from parent index' do
     instructor = Instructor.create!(name: "Olga", subject: "skiing", teaches_children: false, years_experience: 5)
     visit '/instructors'
 
