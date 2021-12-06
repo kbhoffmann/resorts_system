050d6851a152b7f337e0f1d28940a037af39b8aa
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 15:56:31 -0700

    add before each block and add sad paths and more expectations

--- spec/features/pass_holders/show_spec.rb
+++ spec/features/pass_holders/show_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'the pass holders show page' do
-  @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
-  @dana = @breck.pass_holders.create!(name: 'Dana', age: 31, level: 'intermediate', season_pass: true)
-  @kerri = @park_city.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
+
+  before(:each) do
+    @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
+    @kerri = @breck.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
+    @rob = @breck.pass_holders.create!(name: 'Rob', age: 40, level: 'advanced', season_pass: false)
+    @dana = @breck.pass_holders.create!(name: 'Dana', age: 31, level: 'intermediate', season_pass: false)
+  end
 
   it 'displays the pass holder attributes' do
-    visit "/pass_holders/#{kerri.id}"
+    visit "/pass_holders/#{@kerri.id}"
 
-    expect(page).to have_content(kerri.id)
-    expect(page).to have_content(kerri.name)
-    expect(page).to have_content(kerri.level)
-    expect(page).to have_content(kerri.age)
-    expect(page).to have_content(kerri.season_pass)
-    expect(page).to have_content(kerri.created_at)
-    expect(page).to have_content(kerri.updated_at)
+    expect(page).to have_content(@kerri.id)
+    expect(page).to have_content(@kerri.name)
+    expect(page).to have_content(@kerri.level)
+    expect(page).to have_content(@kerri.age)
+    expect(page).to have_content(@kerri.season_pass)
+    expect(page).to have_content(@kerri.created_at)
+    expect(page).to have_content(@kerri.updated_at)
 
-    expect(page).to_not have_content(dana.name)
+    expect(page).to_not have_content(@dana.name)
+    expect(page).to_not have_content(@rob.age)
+    expect(page).to_not have_content(@dana.name)
+    expect(page).to_not have_content(@rob.age)
   end
 end
