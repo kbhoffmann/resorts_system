bfe516c36c65eefd12c91a526e82b28e28c987b6
Hannah Warren <hannahkwarren@gmail.com>
2021-12-06 16:25:22 -0500

    Move tests that check for the link existing on the index page to index_spec.rb

--- spec/features/instructors/edit_spec.rb
+++ spec/features/instructors/edit_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Instructor edit' do
 
-  it 'links to the edit page' do
-    instructor = Instructor.create!(name: "Olga", subject: "skiing", teaches_children: false, years_experience: 5)
-    visit '/instructors'
-
-    click_button("Edit #{instructor.name}")
-    expect(current_path).to eq("/instructors/#{instructor.id}/edit")
-  end
-
   it 'can edit and update the instructor information' do
     instructor = Instructor.create!(name: "Olg", subject: "skiing", teaches_children: false, years_experience: 5)
 
@@ -18,11 +10,13 @@ RSpec.describe 'Instructor edit' do
     expect(page).to have_content("Olg")
     click_button("Edit Olg")
 
-    page.should have_field("Name", with: "Olg")
+    expect(page).to have_field("Name", with: "Olg")
     fill_in(:name, with: "Olga")
     click_button("Update Instructor")
 
     expect(current_path).to eq("/instructors")
     expect(page).to have_content("Olga")
   end
+
+
 end
