5da32e2255768d1b7ecd91e1a66999de068fa129
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 15:52:09 -0700

    add before each block and add sad paths and more expectations

--- spec/features/pass_holders/index_spec.rb
+++ spec/features/pass_holders/index_spec.rb
@@ -1,36 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Pass holders index" do
-  it 'shows all pass holders' do
-    resort = Resort.create!(name: "Winter Park", runs: 20, ski_only: true, city: "Winter Park")
-    pass_holder1 = resort.pass_holders.create!(name: "Hannah Warren", level: "beginner", age: 21, season_pass: true)
-    pass_holder2 = resort.pass_holders.create!(name: "Kerri Hoffmann", level: "advanced", age: 21, season_pass: true)
-
-    visit '/pass_holders'
-
-    expect(page).to have_content(pass_holder1.name)
-    expect(page).to have_content(pass_holder1.level)
-    expect(page).to have_content(pass_holder1.age)
-    expect(page).to have_content(pass_holder1.season_pass)
-    expect(page).to have_content(pass_holder1.created_at)
-    expect(page).to have_content(pass_holder1.updated_at)
-
-    expect(page).to have_content(pass_holder2.name)
-    expect(page).to have_content(pass_holder2.level)
-    expect(page).to have_content(pass_holder2.age)
-    expect(page).to have_content(pass_holder2.season_pass)
-    expect(page).to have_content(pass_holder2.created_at)
-    expect(page).to have_content(pass_holder2.updated_at)
+  before(:each) do
+    @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
+    @kerri = @breck.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
+    @rob = @breck.pass_holders.create!(name: 'Rob', age: 40, level: 'advanced', season_pass: false)
+    @dana = @breck.pass_holders.create!(name: 'Dana', age: 31, level: 'intermediate', season_pass: false)
   end
 
   it 'shows only records where season_pass is true' do
-    resort = Resort.create!(name: "Winter Park", runs: 20, ski_only: true, city: "Winter Park")
-    pass_holder1 = resort.pass_holders.create!(name: "Hannah Warren", level: "beginner", age: 21, season_pass: false)
-    pass_holder2 = resort.pass_holders.create!(name: "Kerri Hoffmann", level: "advanced", age: 21, season_pass: true)
-
     visit '/pass_holders'
 
-    expect(page).to have_content(pass_holder2.name)
-    expect(page).to_not have_content(pass_holder1)
+    expect(page).to have_content(@kerri.name)
+    expect(page).to have_content(@kerri.level)
+    expect(page).to have_content(@kerri.age)
+    expect(page).to have_content(@kerri.season_pass)
+    expect(page).to have_content(@kerri.created_at)
+    expect(page).to have_content(@kerri.updated_at)
+
+    expect(page).to_not have_content(@dana.name)
+    expect(page).to_not have_content(@dana.level)
+    expect(page).to_not have_content(@dana.age)
+    expect(page).to_not have_content(@dana.season_pass)
+    expect(page).to_not have_content(@rob.name)
+    expect(page).to_not have_content(@rob.level)
+    expect(page).to_not have_content(@rob.age)
+    expect(page).to_not have_content(@rob.season_pass)
   end
 end
