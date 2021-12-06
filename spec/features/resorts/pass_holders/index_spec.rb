60d132050f93bb879b5f9e464897d839aad27a1b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 15:11:43 -0700

    add another resort, Park City, and add a sad path to 'shows all names of passholders

--- spec/features/resorts/pass_holders/index_spec.rb
+++ spec/features/resorts/pass_holders/index_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe 'Resorts pass holder index' do
 
   before(:each) do
     @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
+    @park_city = Resort.create!(name: 'Park City', city: 'Park City', runs: 30, ski_only: false)
     @jerry = @breck.pass_holders.create!(name: 'Jerry', age: 63, level: 'intermediate', season_pass: true)
     @kevin = @breck.pass_holders.create!(name: 'Kevin', age: 43, level: 'intermediate', season_pass: true)
-    @kerri = @breck.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
     @dana = @breck.pass_holders.create!(name: 'Dana', age: 31, level: 'intermediate', season_pass: true)
+    @kerri = @park_city.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
   end
 
   it 'shows all of the names for pass_holders at a given resort' do
@@ -15,6 +16,7 @@ RSpec.describe 'Resorts pass holder index' do
 
     expect(page).to have_content(@jerry.name)
     expect(page).to have_content(@kevin.name)
+    expect(page).to_not have_content(@kerri.name)
   end
 
   it 'has a link to sort all the passholders in alphabetical order' do
