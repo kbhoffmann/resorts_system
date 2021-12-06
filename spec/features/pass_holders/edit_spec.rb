4343b3ab7de64ec032b952be3a2a15fa4a7945cb
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 16:06:18 -0700

    add before each bloch and sad path expectations

--- spec/features/pass_holders/edit_spec.rb
+++ spec/features/pass_holders/edit_spec.rb
@@ -1,54 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'the Passholder Update' do
+  before(:each) do
+    @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
+    @kerri = @breck.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
+    @rob = @breck.pass_holders.create!(name: 'Rob', age: 40, level: 'advanced', season_pass: false)
+    @hannah = @breck.pass_holders.create!(name: 'Hannahh', age: 31, level: 'intermediate', season_pass: false)
+  end
+
   it 'links to edit passholder information page' do
-    resort = Resort.create!(name: "Winter Park", runs: 20, ski_only: true, city: "Winter Park")
-    pass_holder = resort.pass_holders.create!(name: "Hannah Warren", level: "beginner", age: 21, season_pass: false)
-    # pass_holder2 = resort.pass_holders.create!(name: "Kerri Hoffmann", level: "advanced", age: 21, season_pass: false)
 
-    visit "/pass_holders/#{pass_holder.id}"
+    visit "/pass_holders/#{@rob.id}"
 
-    click_button "Update #{pass_holder.name}"
+    click_button "Update #{@rob.name}"
 
-    expect(current_path).to eq("/pass_holders/#{pass_holder.id}/edit")
+    expect(current_path).to eq("/pass_holders/#{@rob.id}/edit")
+    expect(current_path).to_not eq("/pass_holders/#{@hannah.id}/edit")
+    expect(current_path).to_not eq("/pass_holders/#{@kerri.id}/edit")
   end
 
   it 'can edit the passholder' do
-    resort = Resort.create!(name: "Winter Park", runs: 20, ski_only: true, city: "Winter Park")
-    pass_holder = resort.pass_holders.create!(name: "Hannah", level: "beginner", age: 21, season_pass: false)
 
-    visit "/pass_holders/#{pass_holder.id}"
+    visit "/pass_holders/#{@hannah.id}"
 
-    expect(page).to have_content('Hannah')
-    expect(page).to have_content('beginner')
-    expect(page).to have_content(21)
+    expect(page).to have_content('Hannahh')
+    expect(page).to have_content('intermediate')
+    expect(page).to have_content(31)
     expect(page).to have_content(false)
+    expect(page).to_not have_content("Kerri")
+    expect(page).to_not have_content("Rob")
 
     click_button 'Update Hannah'
 
-    fill_in 'Name', with: 'Hannah Warren'
-    fill_in 'Level', with: 'intermediate'
-    fill_in 'Age', with: 33
+    fill_in 'Name', with: 'Hannah'
+    fill_in 'Level', with: 'beginner'
+    fill_in 'Age', with: 23
     fill_in 'season_pass', with: true
 
     click_button "Update Passholder"
 
-    expect(current_path).to eq("/pass_holders/#{pass_holder.id}")
-    expect(page).to have_content('Hannah Warren')
-    expect(page).to have_content('intermediate')
-    expect(page).to have_content(33)
+    expect(current_path).to eq("/pass_holders/#{@hannah.id}")
+    expect(current_path).to_not eq("/pass_holders/#{@kerri.id}")
+    expect(current_path).to_not eq("/pass_holders/#{@rob.id}")
+    expect(page).to have_content('Hannah')
+    expect(page).to have_content('beginner')
+    expect(page).to have_content(23)
     expect(page).to have_content(true)
+    expect(page).to_not have_content("Hannahh")
+    expect(page).to_not have_content('intermediate')
+    expect(page).to_not have_content(31)
+    expect(page).to_not have_content(false)
+    expect(page).to_not have_content("Kerri")
+    expect(page).to_not have_content("Rob")
   end
 end
-
-# User Story 14, Child Update (x2)
-# ----------------------------
-# When I visit a Child Show page
-# Then I see a link to update that Child "Update Child"
-# When I click the link
-# I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
-
-# When I click the button to submit the form "Update Child"
-# Then a `PATCH` request is sent to '/child_table_name/:id',
-# the child's data is updated,
-# and I am redirected to the Child Show page where I see the Child's updated information
