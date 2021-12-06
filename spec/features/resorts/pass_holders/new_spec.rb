60e7f025be5539fc7e32fc2f6a1c9a30865658a0
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 15:32:49 -0700

    make before each block and add more expectations for sad paths

--- spec/features/resorts/pass_holders/new_spec.rb
+++ spec/features/resorts/pass_holders/new_spec.rb
@@ -1,30 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'the Pass Holder Creation' do
-  it 'links to the new page from the resort passholders index' do
-    resort = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false )
 
-    visit "/resorts/#{resort.id}/resort_pass_holders"
+  before(:each) do
+    @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
+    @park_city = Resort.create!(name: 'Park City', city: 'Park City', runs: 30, ski_only: false)
+  end
+
+  it 'links to the new passholder form page from the resort passholders index' do
+
+    visit "/resorts/#{@breck.id}/resort_pass_holders"
 
     click_link('New Passholder')
-  
-    expect(current_path).to eq("/resorts/#{resort.id}/resort_pass_holders/new")
+
+    expect(current_path).to eq("/resorts/#{@breck.id}/resort_pass_holders/new")
+    expect(current_path).to_not eq("/resorts/#{@park_city.id}/resort_pass_holders/new")
   end
 
   it 'can create a new passholder' do
-    resort = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false )
-    passholder = resort.pass_holders.create!(name: "Kelsey", age: 38, level: 'intermediate', season_pass: true)
 
-    visit "/resorts/#{resort.id}/resort_pass_holders/new"
+    visit "/resorts/#{@breck.id}/resort_pass_holders/new"
 
-    fill_in('Name', with: "Kelsey")
+    fill_in('name', with: "Kelsey")
+    fill_in('age', with: 38)
+    fill_in('level', with: "intermediate")
+    fill_in('season_pass', with: true)
+
     click_button('Create New Passholder')
 
-    expect(current_path).to eq("/resorts/#{resort.id}/resort_pass_holders")
+    expect(current_path).to eq("/resorts/#{@breck.id}/resort_pass_holders")
+    expect(current_path).to_not eq("/resorts/#{@park_city.id}/resort_pass_holders")
 
     expect(page).to have_content("Kelsey")
     expect(page).to have_content(38)
     expect(page).to have_content("intermediate")
     expect(page).to have_content(true)
+    expect(page).to_not have_content(false)
   end
 end
