1f650f53956d404d9474f1c1f2efd73109998043
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-06 13:47:08 -0700

    Edit: change sorting in index to call active record method from pass_holder model

--- app/controllers/resort_pass_holders_controller.rb
+++ app/controllers/resort_pass_holders_controller.rb
@@ -3,9 +3,10 @@ class ResortPassHoldersController < ApplicationController
   def index
     @resort = Resort.find(params[:resort_id])
     if params[:sort]
-      @pass_holders = PassHolder.order(name: params[:sort])
+      @pass_holders = @pass_holders.sort_name_alphabetically
     else
       @pass_holders = @resort.pass_holders
+      #@students = @instructor.students.all
     end
 
   end
