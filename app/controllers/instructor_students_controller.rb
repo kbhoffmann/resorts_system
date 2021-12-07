e9d11f190a0d685231302b3cd4db2c86c67e3c1d
Hannah Warren <hannahkwarren@gmail.com>
2021-12-06 22:06:41 -0500

    Add form with filtering ability for students on an instructor_students index page

--- app/controllers/instructor_students_controller.rb
+++ app/controllers/instructor_students_controller.rb
@@ -1,8 +1,13 @@
 class InstructorStudentsController < ApplicationController
   def index
     @instructor = Instructor.find(params[:instructor_id])
+
     if params[:sort]
       @students = @instructor.students.by_name
+    elsif params[:min_age]
+      @students = @instructor.students.min_age(params[:min_age]) 
+    elsif params[:sort] && params[:min_age]
+      @students = @instructor.students.by_name.min_age(params[:min_age])
     else
       @students = @instructor.students.all
     end
@@ -18,6 +23,10 @@ class InstructorStudentsController < ApplicationController
     redirect_to "/instructors/#{@instructor.id}/students"
   end
 
+  # def filter 
+  #   @instructor = Instructor.find(params[:instructor_id])
+  #   @students = @instructor.students.min_age(params[:min_age])
+  # end
 
 private
   def instructor_students_params
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -21,5 +21,14 @@ RSpec.describe "Student model" do
     expect(Student.by_name).to eq([dana, jim, tommy])
   end
 
+  it 'filters students by user-provided age' do
+    gretchen = Instructor.create!(name: 'Gretchen', subject: "cross-country skiing", teaches_children: true, years_experience:10)
+    tommy = gretchen.students.create!(name: 'Tommy', age: 18, subject: "cross-country skiing", returning_student: true, level: "intermediate")
+    dana = gretchen.students.create!(name: 'Dana', age: 14, subject: "cross-country skiing", returning_student: true, level: "intermediate")
+    jim = gretchen.students.create!(name: 'Jim', age: 21, subject: "cross-country skiing", returning_student: false, level: "intermediate")
+
+    expect(Student.min_age(18)).to match_array([tommy, jim])
+  end
+
 
 end
