cfc5743373df221c975f8570fc0b75aabe2ea8b1
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-07 17:11:16 -0700

    add form to add filter by age functionality to passholder by resort id

--- app/models/pass_holder.rb
+++ app/models/pass_holder.rb
@@ -8,4 +8,8 @@ class PassHolder < ApplicationRecord
   def self.true_only
     where(season_pass: true)
   end
+
+  def self.minimum_age(min_age)
+    where("age > #{min_age}")
+  end
 end
--- spec/features/resorts/pass_holders/index_spec.rb
+++ spec/features/resorts/pass_holders/index_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'Resorts pass holder index' do
   before(:each) do
     @breck = Resort.create!(name: 'Breckenridge', city: 'Breckenridge', runs: 20, ski_only: false)
     @park_city = Resort.create!(name: 'Park City', city: 'Park City', runs: 30, ski_only: false)
-    @jerry = @breck.pass_holders.create!(name: 'Jerry', age: 63, level: 'intermediate', season_pass: true)
-    @kevin = @breck.pass_holders.create!(name: 'Kevin', age: 43, level: 'intermediate', season_pass: true)
-    @dana = @breck.pass_holders.create!(name: 'Dana', age: 31, level: 'intermediate', season_pass: true)
+    @jerry = @breck.pass_holders.create!(name: 'Jerry', age: 33, level: 'intermediate', season_pass: true)
+    @kevin = @breck.pass_holders.create!(name: 'Kevin', age: 25, level: 'intermediate', season_pass: true)
+    @dana = @breck.pass_holders.create!(name: 'Dana', age: 17, level: 'intermediate', season_pass: true)
     @kerri = @park_city.pass_holders.create!(name: 'Kerri', age: 11, level: 'beginner', season_pass: true)
   end
 
@@ -26,5 +26,19 @@ RSpec.describe 'Resorts pass holder index' do
 
     expect(@dana.name).to appear_before(@jerry.name)
     expect(@jerry.name).to_not appear_before(@dana.name)
+  end
+
+  it 'has a form to limit the amount of records displayed' do
+    visit "/resorts/#{@breck.id}/resort_pass_holders"
+
+    fill_in :min_age, with: 17
+
+    click_on "Return records with pass holders greater than specified age"
+
+    expect(current_path).to eq("/resorts/#{@breck.id}/resort_pass_holders")
+    expect(page).to_not have_content('Dana')
+    expect(page).to_not have_content('Kerri')
+    expect(page).to have_content('Jerry')
+    expect(page).to have_content('Kevin')
   end
 end
