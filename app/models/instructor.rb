e46e369906e34d53ef3c38e776aa6a34453e474f
Hannah Warren <hannahkwarren@gmail.com>
2021-12-08 17:48:18 -0500

    Adds a nuance from User Story 7, student count for each instructor on the index page

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,6 +4,10 @@ class Instructor < ApplicationRecord
   def self.ordered_by_created_at
     order(created_at: :desc)
   end
+  
+  def students_count 
+    self.students.count
+  end
 
   def self.ordered_by_student_count
     left_joins(:students).group(:id).order(Arel.sql("COUNT(students.id) DESC"))
